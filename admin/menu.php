0354be7f38f807141f17d32de4b29102fff582bb
PhamHuy <mewtwoclub1@gmail.com>
2023-11-15 22:03:20 +0700

    Upload giao dien admin

--- admin/menu.php
+++ admin/menu.php
@@ -5,17 +5,17 @@
                         <a href="index.php"><i class="fa fa-dashboard"></i> Dashboard</a>
                     </li>
                     <li>
-                        <a href=""><i class="fa fa-desktop"></i> Quản lý sản phẩm</a>
+                        <a href="?act=dssp"><i class="fa fa-desktop"></i> Quản lý sản phẩm</a>
                     </li>
 					<li>
                         <a href="?act=dsdm"><i class="fa fa-bar-chart-o"></i> Quản lý danh mục</a>
                     </li>
                     <li>
-                        <a href="#"><i class="fa fa-qrcode"></i> Quản lý tài khoản</a>
+                        <a href="?act=dskh"><i class="fa fa-qrcode"></i> Quản lý tài khoản</a>
                     </li>
                     
                     <li>
-                        <a href="#"><i class="fa fa-table"></i> Quản lý bình luận</a>
+                        <a href="?act=dsbl"><i class="fa fa-table"></i> Quản lý bình luận</a>
                     </li>
                     <li>
                         <a href="#"><i class="fa fa-edit"></i> Quản lý đơn hàng</a>
